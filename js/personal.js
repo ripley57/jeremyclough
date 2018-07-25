4231d639042be9ef0913801a849f648a6657ae47
Jeremy <clough.jeremy@gmail.com>
2018-07-25 20:04:02 +0100

    Add latest changes including using JS promises and jQuery deferreds

--- js/personal.js
+++ js/personal.js
@@ -1,46 +1,92 @@
 
-		function decryptImage(image_path,placeholder) {
-			jQuery.get(image_path, function(data) {
-				var password = document.getElementById('password').value;
-				var decrypted = CryptoJS.AES.decrypt(data.split(/\s/).join(''), password).toString(CryptoJS.enc.Base64);
-				var newImg = document.createElement("img");
-				
-				newImg.setAttribute('alt','If you can see this then the image decryption failed!');
-				newImg.setAttribute('src', 'data:image/jpg;base64,' + decrypted);
-				
-				// Start with image being hidden. 
-				//newImg.setAttribute('style', 'display:none');
-				//newImg.setAttribute('class', 'fadein');
-				newImg.setAttribute('border', '2');
-				
-				newImg.addEventListener('error', function(){ 
-					// Image decryption failed.
-					document.getElementById("decryption_feedback").innerHTML = "Decryption failed!";
-					// Blink the feedback message.
-					$(".blink").animate({opacity:0},200,"linear",function(){
-					$(this).animate({opacity:1},200);
+	function getEncryptedImageData(url) {
+		const deferred = new $.Deferred(function(resolve, reject) {
+	 		const request = new XMLHttpRequest();
+			request.open('GET', url);
+			request.onload = function() {
+				if (request.status === 200) {
+					deferred.resolve(request.response);
+				} else {
+					deferred.reject(Error(request.statusText));
+				}
+			};
+			request.onerror = function() {
+				reject(Error('Error fetching data.'));
+			};
+			// This triggers the asynchronous GET request.
+			request.send();
+		});	
+		return deferred.promise();
+	}
+
+	function getDecryptedImageData(encrypted_data,password) {
+		const deferred = new $.Deferred(function(resolve, reject) {
+			// Resolve the promise. The following is synchronous AFAIK, so we are 
+			// using a promise here simply so that we can chain our function calsl using "then".
+			var decrypted_data = CryptoJS.AES.decrypt(encrypted_data.split(/\s/).join(''), password).toString(CryptoJS.enc.Base64);
+			// We need to wrap this in a timeout to prevent "use before declaration" run-time error.
+			setTimeout(function(){deferred.resolve(decrypted_data);}, 100);
+		});
+		return deferred.promise();
+	}
+			
+	function displayDecryptedImage(decrypted_data,placeholder) {
+		const deferred = new $.Deferred(function(resolve, reject) {
+			var newImg = document.createElement("img");
+			newImg.setAttribute('alt','If you can see this then the image decryption probably failed!');
+			newImg.setAttribute('style', 'display:none'); // Start with image hidden.
+			newImg.setAttribute('class', 'fadein'); // So we can later un-hide the image.
+			newImg.setAttribute('border', '2'); // Add image border to clearly indicate each page.
+			newImg.onload = function() { 
+				document.getElementById(placeholder).appendChild(newImg);
+				deferred.resolve("Image decryption and display successful");
+			};
+			newImg.onerror = function() {
+				console.log('Unable to display image. Decryption password is most likely incorrect.');
+				deferred.reject(Error('Unable to display image'));
+			};
+			// I suspect this is what triggers the image loading.
+			newImg.setAttribute('src', 'data:image/jpg;base64,' + decrypted_data);
+		});
+		return deferred.promise();
+	}
+			
+	function decryptAndDisplaySingleImage(url,password,placeholder) {
+		const deferred = new $.Deferred(function(resolve, reject) {
+			var deferred_1 = getEncryptedImageData(url);
+			deferred_1.done(function(encrypted_data) {
+				var deferred_2 = getDecryptedImageData(encrypted_data,password);
+				deferred_2.done(function(decrypted_data) {
+					var deferred_3 = displayDecryptedImage(decrypted_data,placeholder);
+					deferred_3.done(function() {
+						console.log('Image displayed successfully: ' + basename(url));
+						deferred.resolve('Image displayed successfully: ' + basename(url));
+					})
+					.fail(function() {
+						deferred.reject(Error('Unable to display image: ' + basename(url)));
 					});
-					// Clear the feedback message.
-					setTimeout(function(){
-					document.getElementById("decryption_feedback").innerHTML = "";
-					}, 2000);
+				})
+				.fail(function() {
+					deferred.reject(Error('Unable to decrypt image data: ' + basename(url)));
 				});
-				if (newImg.complete) {
-					// Image decryption successful.
-					document.getElementById(placeholder).appendChild(newImg);
-					// Display the image.
-					//$('.fadein').toggle('slow');
-					// Assume all images on this page will decrypt successfully,
-					// which means we can safely Hide the password-entry form.
-					$("#form_decrypt").toggle();
-				}
-				else {
-					newImg.addEventListener('load', function(){ 
-					// Image decryption successful.
-					document.getElementById(placeholder).appendChild(newImg);
-					// Display the image.
-					//$('.fadein').toggle('slow');
-					});
-				}
+			})
+			.fail(function() {
+				deferred.reject(Error('Unable to get image data: ' + basename(url)));
 			});
-		}
+		});
+		return deferred.promise();
+	}
+
+	function setFeedback(txt) {
+		$('#decryption_feedback').html(txt);
+	}
+	function clearFeedback() {
+		setFeedback('');
+	}
+
+	function basename(str) {
+   		var base = new String(str).substring(str.lastIndexOf('/') + 1); 
+    		if(base.lastIndexOf(".") != -1)       
+        		base = base.substring(0, base.lastIndexOf("."));
+   		return base;
+	}
--- pages/samples/sample_1.md
+++ pages/samples/sample_1.md
@@ -7,28 +7,44 @@ title: Samples
 <div id="content_body">
 <h1>Sample 1</h1>
 
-	<form id="form_decrypt" name="f">
-	Password:&nbsp;<input type="text" id="password" size="20" value="" />
-	<input type="button" onclick="decryptAllImages();" value="Decrypt" />
-	<p id="decryption_feedback" class="blink"></p>
-	</form>
+{% include sample_form.html %}
 
-	<div id="placeholder1"></div>
-	<div id="placeholder2"></div>
-	<div id="placeholder3"></div>
+<div id="placeholder1"></div>
+<div id="placeholder2"></div>
+<div id="placeholder3"></div>
 </div>
 </div>
-	<script type="text/javascript">
-	function decryptAllImages() {
-		document.getElementById("decryption_feedback").innerHTML = "";
 
-		decryptImage('../../assets/images/samples/SAMP01-1.jpg.encrypted','placeholder1');
-		decryptImage('../../assets/images/samples/SAMP01-2.jpg.encrypted','placeholder2');
-		decryptImage('../../assets/images/samples/SAMP01-3.jpg.encrypted','placeholder3');
-	}
-	</script>
+<script type="text/javascript">
+function decryptAllImages() {
+        setFeedback('Decrypting ...');
+	var password = document.getElementById('password').value;
+	var def1 = decryptAndDisplaySingleImage('../../assets/images/samples/SAMP01-1.jpg.encrypted',password,'placeholder1');
+	def1.fail(function(err) {
+		setFeedback(err);
+	});
+	def1.done(function() {
+		var def2 = decryptAndDisplaySingleImage('../../assets/images/samples/SAMP01-2.jpg.encrypted',password,'placeholder2');
+		def2.fail(function(err) {
+			setFeedback(err);	
+		});
+		def2.done(function() {
+			var def3 = decryptAndDisplaySingleImage('../../assets/images/samples/SAMP01-3.jpg.encrypted',password,'placeholder3');
+			def3.fail(function(err) {
+				setFeedback(err);	
+			});
+			def3.done(function() {
+				// All images successfully decrypted and displayed. 
+				// Which means we can now hide the password form.
+				$("#form_password").toggle();
+				// And let's now un-hide the images.
+				$('.fadein').toggle('slow');
+			});
+		});
+	});
+}
+</script>
 
-    {% include samples_list.html %}
-
-    {% include sample_active.html sample_id="'sample1'" %}
+{% include samples_list.html %}
+{% include sample_active.html sample_id="'sample1'" %}
 </div>
