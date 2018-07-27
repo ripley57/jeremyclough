fd7a186ba6d8f4336a2eb3fa5db9e5670d904e22
Jeremy <ripley57@users.noreply.github.com>
2018-07-27 12:26:12 +0100

    Update encrypt_images.sh

--- encrypt_images.sh
+++ encrypt_images.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+#
+# Procedure to create a new enrypted image:
+# 1) Capture the image in a high res bitmap format, e.g. tif.
+# 2) Use PSP to resize the image (e.g. tif), to 650x841 pixels.
+# 3) Use ImageMagick to convert the image to a jpg.
+# 4) Run this script on the jpg to encrypt it.
 
 script_name=$(basename $0)
 
